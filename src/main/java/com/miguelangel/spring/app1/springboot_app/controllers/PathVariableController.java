2dc763db73a47c42ea2e76ae2fe78c383e50d590
Angixs-zz <hpma.305@gmail.com>
2026-07-16 19:00:29 -0600

    Implementar PathVariable y endpoint POST con TacoDTO

---
+++ src/main/java/com/miguelangel/spring/app1/springboot_app/controllers/PathVariableController.java
@@ -0,0 +1,32 @@
+package com.miguelangel.spring.app1.springboot_app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.miguelangel.spring.app1.springboot_app.models.dto.TacoDTO;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/variable")
+public class PathVariableController {
+
+    @GetMapping("/pagina1/{mensaje}")
+    public ParametroDTO pagina1(@PathVariable String mensaje) {
+
+        ParametroDTO parametro1 = new ParametroDTO();
+        parametro1.setInformacion(mensaje);
+
+        return parametro1;
+    }
+
+    @PostMapping("/solicitud")
+    public TacoDTO crearTaco(@RequestBody TacoDTO taco) {
+
+        return taco;
+    }
+}
--- src/main/java/com/miguelangel/spring/app1/springboot_app/models/dto/TacoDTO.java
+++ src/main/java/com/miguelangel/spring/app1/springboot_app/models/dto/TacoDTO.java
@@ -1,19 +1,34 @@
 package com.miguelangel.spring.app1.springboot_app.models.dto;
+
 public class TacoDTO {
 
     private int id;
     private String nombre;
     private String tipoCarne;
+    private String tipoTortilla;
+    private String tipoSalsa;
     private int precio;
+    private int cantidad;
 
     public TacoDTO() {
     }
 
-    public TacoDTO(int id, String nombre, String tipoCarne, int precio) {
+    public TacoDTO(
+            int id,
+            String nombre,
+            String tipoCarne,
+            String tipoTortilla,
+            String tipoSalsa,
+            int precio,
+            int cantidad) {
+
         this.id = id;
         this.nombre = nombre;
         this.tipoCarne = tipoCarne;
+        this.tipoTortilla = tipoTortilla;
+        this.tipoSalsa = tipoSalsa;
         this.precio = precio;
+        this.cantidad = cantidad;
     }
 
     public int getId() {
@@ -40,11 +55,35 @@ public class TacoDTO {
         this.tipoCarne = tipoCarne;
     }
 
+    public String getTipoTortilla() {
+        return tipoTortilla;
+    }
+
+    public void setTipoTortilla(String tipoTortilla) {
+        this.tipoTortilla = tipoTortilla;
+    }
+
+    public String getTipoSalsa() {
+        return tipoSalsa;
+    }
+
+    public void setTipoSalsa(String tipoSalsa) {
+        this.tipoSalsa = tipoSalsa;
+    }
+
     public int getPrecio() {
         return precio;
     }
 
     public void setPrecio(int precio) {
         this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
     }
 }
