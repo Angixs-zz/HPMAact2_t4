263164ed8a0740ab8035cdaf73452a5d53bd1cfb
Angixs-zz <hpma.305@gmail.com>
2026-07-16 19:26:15 -0600

    Implementar Value con propiedades personalizadas

--- src/main/java/com/miguelangel/spring/app1/springboot_app/controllers/PathVariableController.java
+++ src/main/java/com/miguelangel/spring/app1/springboot_app/controllers/PathVariableController.java
@@ -5,15 +5,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.miguelangel.spring.app1.springboot_app.models.dto.TacoDTO;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
 @RequestMapping("/api/variable")
 public class PathVariableController {
+
+    @Value("${config.usuario}")
+    private String usuario;
+    @Value("${config.codigo}")
+    private int codigo;
+    @Value("${config.mensaje}")
+    private String mensaje;
+    @Value("${config.valores}")
+    private String[] valores;
+
+
 
     @GetMapping("/pagina1/{mensaje}")
     public ParametroDTO pagina1(@PathVariable String mensaje) {
@@ -29,4 +46,15 @@ public class PathVariableController {
 
         return taco;
     }
+
+    @GetMapping("/valores")
+    public Map<String, Object> values(){
+        Map<String, Object>json = new HashMap<>();
+        json.put("usuario", usuario);
+        json.put("codigo", codigo);
+        json.put("mensaje", mensaje);
+        json.put("valores", valores);
+        return json;
+    }
+    
 }
